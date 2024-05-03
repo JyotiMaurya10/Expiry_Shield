a8efd4e3518d228e799554341f99c7445cd1a50f
JyotiMaurya10 <153389813+JyotiMaurya10@users.noreply.github.com>
2024-05-03 15:23:29 +0530

    qr generater

--- Expiry_Shield/lib/utils/app_constant.dart
+++ Expiry_Shield/lib/utils/app_constant.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 class AppConstant {
   static String appMainName = 'Expiry Shield';
   static String appPoweredBy = 'Everything is Safe now';
-  // static const appMainColor = Color.fromARGB(255, 158, 158, 158);
   static const appMainColor = Color(0xFFbf1b08);
-  // static const appSecondaryColor = Colors.grey;
   static const appSecondaryColor = Color(0xFF981206);
   static const appTextColor = Color(0xFFFBF5F4);
   static const appStatusBarColor = Color(0xFFFBF5F4);
